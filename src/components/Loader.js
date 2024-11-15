9a38841d1d6e3513763f019f3c3d1276d24419f9
Ajay <ajay.raj@roadelabs.com>
2024-11-15 13:02:57 +0530

    updated loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -45,7 +45,34 @@ function Loader({ type, progress }) {
                             </div>
                         </div>
                     ) : (
-                        <CircularProgress />
+                        <>
+                            <svg width={0} height={0}>
+                                <defs>
+                                    <linearGradient
+                                        id="my_gradient"
+                                        x1="0%"
+                                        y1="0%"
+                                        x2="0%"
+                                        y2="100%"
+                                    >
+                                        <stop offset="0%" stopColor="#e01cd5" />
+                                        <stop
+                                            offset="100%"
+                                            stopColor="#1CB5E0"
+                                        />
+                                    </linearGradient>
+                                </defs>
+                            </svg>
+                            <CircularProgress
+                                thickness={4}
+                                size="5rem"
+                                sx={{
+                                    "svg circle": {
+                                        stroke: "url(#my_gradient)",
+                                    },
+                                }}
+                            />
+                        </>
                     )}
                 </div>,
                 el
