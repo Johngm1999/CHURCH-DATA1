d7ce47fcafe60bd6eaa2fa2ef84eac0d06b6645c
Ajay <ajay.raj@roadelabs.com>
2024-11-26 10:04:49 +0530

    Maintenance notice added

--- src/views/login/UserLogin.js
+++ src/views/login/UserLogin.js
@@ -17,6 +17,7 @@ import extractErrorFromRes from "../../helpers/extractErrorFromRes";
 import Loader from "../../components/Loader";
 import toast from "react-hot-toast";
 import churchImg from "../../asset/img/church.jpeg";
+import MaintenanceNotice from "../../components/MaintenanceNotice";
 
 function Login() {
     const [visible, setVisible] = useState(false);
@@ -77,82 +78,9 @@ function Login() {
                 container
                 justifyContent="center"
                 alignItems="center"
-                sx={{ minHeight: "90vh", pr: 35 }}
+                sx={{ minHeight: "90vh" }}
             >
-                <Grid
-                    item
-                    sm={5}
-                    sx={{
-                        p: 4,
-                        borderRadius: 5,
-                        boxShadow: 18,
-                        bgcolor: "#fff",
-                    }}
-                >
-                    <div className="w-75 mx-auto">
-                        <Typography variant="h5">Welcome back</Typography>
-                        <Typography variant="h4" gutterBottom>
-                            Login to your account
-                        </Typography>
-
-                        <form onSubmit={onLogin}>
-                            <TextField
-                                label="Username"
-                                name="USERNAME"
-                                value={userCredential.USERNAME}
-                                onChange={onChangeHandler}
-                                fullWidth
-                                margin="normal"
-                                required
-                                variant="outlined"
-                            />
-                            <TextField
-                                label="Password"
-                                name="PASSWORD"
-                                type={visible ? "text" : "password"}
-                                value={userCredential.PASSWORD}
-                                onChange={onChangeHandler}
-                                fullWidth
-                                margin="normal"
-                                required
-                                variant="outlined"
-                                autoComplete="current-password"
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton
-                                                onClick={
-                                                    togglePasswordVisibility
-                                                }
-                                            >
-                                                {visible ? (
-                                                    <Visibility />
-                                                ) : (
-                                                    <VisibilityOff />
-                                                )}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    ),
-                                }}
-                            />
-
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                fullWidth
-                                sx={{ mt: 2 }}
-                                disabled={loggingIn}
-                            >
-                                {loggingIn ? (
-                                    <CircularProgress size={24} />
-                                ) : (
-                                    "Login"
-                                )}
-                            </Button>
-                        </form>
-                    </div>
-                </Grid>
+                <MaintenanceNotice />
                 {loggingIn && <Loader />}
             </Grid>
         </div>
